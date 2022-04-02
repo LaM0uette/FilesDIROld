abbf5dd4f02c6722c56eb55a1570e8b49f7ee146
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-02 12:49:29 +0200

    création de la loop sur les fichiers

--- build/func.go
+++ build/func.go
@@ -2,6 +2,7 @@ package build
 
 import (
 	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"log"
 	"os"
 	"path/filepath"
@@ -28,8 +29,13 @@ func CurrentDir() string {
 func (s *Search) SearchFiles() error {
 
 	id := 1
-
 	DrawStartSearch()
+	wb := excelize.NewFile()
+	wb.SetCellValue("Sheet1", "A1", "id")
+	wb.SetCellValue("Sheet1", "B1", "Fichier")
+	wb.SetCellValue("Sheet1", "C1", "Date")
+	wb.SetCellValue("Sheet1", "D1", "Lien_Fichier")
+	wb.SetCellValue("Sheet1", "E1", "Lien")
 
 	err := filepath.Walk(s.Path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -44,6 +50,11 @@ func (s *Search) SearchFiles() error {
 			}
 
 			fmt.Printf("N°%v | Fichier: %v\n", id, fileStat.Name())
+			wb.SetCellValue("Sheet1", fmt.Sprintf("A%v", id+1), id)
+			wb.SetCellValue("Sheet1", fmt.Sprintf("B%v", id+1), fileStat.Name())
+			wb.SetCellValue("Sheet1", fmt.Sprintf("C%v", id+1), fmt.Sprintf("%v", fileStat.ModTime()))
+			wb.SetCellValue("Sheet1", fmt.Sprintf("D%v", id+1), path)
+			wb.SetCellValue("Sheet1", fmt.Sprintf("E%v", id+1), filepath.Dir(path))
 			id++
 		}
 
@@ -52,6 +63,10 @@ func (s *Search) SearchFiles() error {
 	if err != nil {
 		fmt.Println(err)
 		return err
+	}
+
+	if err := wb.SaveAs("FilesDIR_Data.xlsx"); err != nil {
+		log.Fatal(err)
 	}
 
 	DrawEndSearch(s.Path, "f", "g", id)
