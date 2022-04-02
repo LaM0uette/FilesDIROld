c258cae257a290948cdbaf15607ef9e7339fabdd
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-02 13:56:19 +0200

    ajout de commentaires

--- FilesDIR.go
+++ FilesDIR.go
@@ -9,12 +9,14 @@ import (
 
 func main() {
 
+	// setup flag for insert data of search in cli
 	schMode := flag.String("m", "%", "Mode de recherche.")
 	schWord := flag.String("f", "", "Non de fichier.")
 	schExt := flag.String("e", "*", "Extension de fichier.")
 	schPath := flag.String("l", build.CurrentDir(), "Extension de fichier.")
 	flag.Parse()
 
+	// generated the structure with data to search for files
 	s := build.Search{
 		Mode:      *schMode,
 		Word:      *schWord,
@@ -22,8 +24,10 @@ func main() {
 		Path:      *schPath,
 	}
 
+	// print on screen the start of program
 	build.DrawStart()
 
+	// print on screen the start of search
 	err := s.SearchFiles()
 	if err != nil {
 		fmt.Println(err)
