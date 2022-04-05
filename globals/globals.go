e855ad3ddfced8813eef645352de7f3378ed8b4f
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-05 20:47:24 +0200

    Ajout du waitGroup

--- globals/globals.go
+++ globals/globals.go
@@ -1,4 +1,4 @@
 package globals
 
 // var SrcPath = "C:\\Users\\doria\\go\\src\\FilesDIR\\tests"
-var SrcPath = "F:\\testBis\\dl\\01_PRET"
+var SrcPath = "F:\\"
--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"FilesDIR/globals"
 	"FilesDIR/task"
+	"fmt"
 	"log"
+	"time"
 )
 
 func main() {
+	timeStart := time.Now()
 
 	task.DrawStart()
 
 	err := task.LoopDir(globals.SrcPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err.Error())
 	}
+
+	timeEnd := time.Since(timeStart)
+	fmt.Println(timeEnd)
 }
