fa37a2f2c60f1e4d92693c76b4c3ea88f53dbdfd
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-06 20:35:25 +0200

    Création des logs

--- task/task_test.go
+++ task/task_test.go
@@ -4,9 +4,11 @@ import (
 	"testing"
 )
 
+const scrTest = "C:\\Users\\doria\\go\\src\\FilesDIR\\tests"
+
 func TestRunSearch(t *testing.T) {
 	s := Sch{
-		SrcPath:  "C:\\Users\\doria\\go\\src\\FilesDIR\\tests",
+		SrcPath:  scrTest,
 		PoolSize: 10,
 		NbFiles:  0,
 	}
@@ -14,5 +16,20 @@ func TestRunSearch(t *testing.T) {
 	RunSearch(&s)
 	if s.NbFiles != 18 {
 		t.Error("NbFiles is not 18", s.NbFiles)
+	}
+}
+
+func BenchmarkRunSearch(b *testing.B) {
+	s := Sch{
+		SrcPath:  scrTest,
+		PoolSize: 10,
+		NbFiles:  0,
+	}
+
+	for i := 0; i < b.N; i++ {
+		RunSearch(&s)
+		if s.NbFiles != 18 {
+			b.Error("NbFiles is not 18", s.NbFiles)
+		}
 	}
 }
