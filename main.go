61368ca3ade458c80728bcf10871c44c9527793f
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-06 19:50:33 +0200

    Boucle ultra rapide en limitant les thread

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"FilesDIR/globals"
 	"FilesDIR/task"
+	"fmt"
+	"log"
+	"os"
 	"time"
 )
 
@@ -17,6 +20,14 @@ func main() {
 		PoolSize: 10,
 		NbFiles:  0,
 	}
+
+	file, err := os.OpenFile(fmt.Sprintf("logs/SLog_%v.txt", timerStart.Format("20060102150405")), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+
+	log.Println("Hello world!")
 
 	task.RunSearch(&s)
 
