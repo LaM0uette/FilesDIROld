a4f5942e53895d9180550c21f8ebe4e822bc5f7d
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-10 18:38:46 +0200

    Ajout du flag FlgCompiler pour lancer la compilatopin des fiches appuis ft

--- construct/construct.go
+++ construct/construct.go
@@ -136,8 +136,8 @@ func (f *Flags) GenerateExcelSave(DstPath string) {
 	}
 
 	wg.Wait() // Wait for all write loops to complete
-	time.Sleep(1 * time.Second)
 	fmt.Printf("\rNombre de lignes sauvegardées :  %v/%v\n", iMax, iMax)
+	time.Sleep(1 * time.Second)
 
 	// Generate a default word if is none
 	saveWord := f.SetSaveWord()
