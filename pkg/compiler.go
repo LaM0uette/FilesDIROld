6166b6c0ca129f7f1d2ff19624e57cc4935e93b8
LaM0uette <lam0uette.dev@gmail.com>
2022-04-11 16:59:00 +0200

    ajout des couleur sur certains cellules

--- pkg/compiler.go
+++ pkg/compiler.go
@@ -6,8 +6,7 @@ import (
 	"FilesDIR/loger"
 	"FilesDIR/task"
 	"fmt"
-	"github.com/qax-os/excelize"
-	"github.com/tealeg/xlsx"
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -24,9 +23,7 @@ type compilData struct {
 var (
 	wg   sync.WaitGroup
 	jobs = make(chan compilData)
-	Wb   = &xlsx.File{}
-	Sht  = &xlsx.Sheet{}
-	Mut  = sync.RWMutex{}
+	Wb   = &excelize.File{}
 	Id   int
 )
 
@@ -38,54 +35,38 @@ func ClsTempFiles() {
 	_ = os.RemoveAll(globals.FolderExports)
 }
 
-func setCellValue(r, c int, val any) {
-	cell, _ := Sht.Cell(r, c)
-	cell.SetValue(val)
-}
-
-func getCellValue(sht *xlsx.Sheet, r, c int) (str string, style *xlsx.Style) {
-	cell, _ := sht.Cell(r, c)
-	return cell.Value, cell.GetStyle()
-}
-
-func setCellBgColor(r, c int, style *xlsx.Style) {
-	cell, _ := Sht.Cell(r, c)
-	cell.SetStyle(style)
-}
-
 func CompilerFicheAppuiFt(path string) {
-	path = "C:\\Users\\XD5965\\OneDrive - EQUANS\\Bureau\\Nouveau dossier"
 
 	loger.BlankDateln(display.DrawInitCompiler())
 	time.Sleep(800 * time.Millisecond)
 
 	loger.Blankln(display.DrawRunCompiler())
 
-	Wb = xlsx.NewFile()
-	Sht, _ = Wb.AddSheet("Sheet1")
+	Id = 1
 
-	setCellValue(0, 0, "Chemin de la fiche")
-	setCellValue(0, 1, "Adresse")
-	setCellValue(0, 2, "Ville")
-	setCellValue(0, 3, "Num appui")
-	setCellValue(0, 4, "Type appui")
-	setCellValue(0, 5, "Type_n_app")
-	setCellValue(0, 6, "Nature TVX")
-	setCellValue(0, 7, "Etiquette jaune")
-	setCellValue(0, 8, "Effort avant ajout câble")
-	setCellValue(0, 9, "Effort après ajout câble")
-	setCellValue(0, 10, "Effort nouveau appui")
-	setCellValue(0, 11, "Latitude")
-	setCellValue(0, 12, "Longitude")
-	setCellValue(0, 13, "Opérateur")
-	setCellValue(0, 14, "Appui utilisable en l'état")
-	setCellValue(0, 15, "Environnement")
-	setCellValue(0, 16, "Commentaire appui")
-	setCellValue(0, 17, "Commentaire global")
-	setCellValue(0, 18, "Proxi ENEDIS")
-	setCellValue(0, 19, "id_metier_")
-	setCellValue(0, 20, "Date")
-	setCellValue(0, 21, "PB")
+	Wb = excelize.NewFile()
+	_ = Wb.SetCellValue("Sheet1", "A1", "Chemin de la fiche")
+	_ = Wb.SetCellValue("Sheet1", "B1", "Adresse")
+	_ = Wb.SetCellValue("Sheet1", "C1", "Ville")
+	_ = Wb.SetCellValue("Sheet1", "D1", "Num appui")
+	_ = Wb.SetCellValue("Sheet1", "E1", "Type appui")
+	_ = Wb.SetCellValue("Sheet1", "F1", "Type_n_app")
+	_ = Wb.SetCellValue("Sheet1", "G1", "Nature TVX")
+	_ = Wb.SetCellValue("Sheet1", "H1", "Etiquette jaune")
+	_ = Wb.SetCellValue("Sheet1", "I1", "Effort avant ajout câble")
+	_ = Wb.SetCellValue("Sheet1", "J1", "Effort après ajout câble")
+	_ = Wb.SetCellValue("Sheet1", "K1", "Effort nouveau appui")
+	_ = Wb.SetCellValue("Sheet1", "L1", "Latitude")
+	_ = Wb.SetCellValue("Sheet1", "M1", "Longitude")
+	_ = Wb.SetCellValue("Sheet1", "N1", "Opérateur")
+	_ = Wb.SetCellValue("Sheet1", "O1", "Appui utilisable en l'état")
+	_ = Wb.SetCellValue("Sheet1", "P1", "Environnement")
+	_ = Wb.SetCellValue("Sheet1", "Q1", "Commentaire appui")
+	_ = Wb.SetCellValue("Sheet1", "R1", "Commentaire global")
+	_ = Wb.SetCellValue("Sheet1", "S1", "Proxi ENEDIS")
+	_ = Wb.SetCellValue("Sheet1", "T1", "id_metier_")
+	_ = Wb.SetCellValue("Sheet1", "U1", "Date")
+	_ = Wb.SetCellValue("Sheet1", "V1", "PB")
 
 	for w := 1; w <= 500; w++ {
 		go workerFicheAppuiFt()
@@ -120,31 +101,30 @@ func CompilerFicheAppuiFt(path string) {
 				r := row[3]
 
 				go func() {
-					Mut.Lock()
 					wg.Add(1)
 					Id++
 
-					jobs <- compilData{
+					a := compilData{
 						Path: r,
 						Id:   Id,
 					}
-					Mut.Unlock()
+
+					jobs <- a
 				}()
 			}
 		}
 	}
 
 	wg.Wait()
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(1 * time.Second)
 
 	loger.BlankDateln(display.DrawEndCompiler())
 
 	loger.BlankDateln(fmt.Sprintf("Nombre de fiches compilées : %v", Id-1))
 	time.Sleep(800 * time.Millisecond)
 
-	err = Wb.Save(filepath.Join(path, fmt.Sprintf("__COMPILATION__%v.xlsx", time.Now().Format("20060102150405"))))
-	if err != nil {
-		return
+	if err := Wb.SaveAs(filepath.Join(path, fmt.Sprintf("__COMPILATION__%v.xlsx", time.Now().Format("20060102150405")))); err != nil {
+		fmt.Println(err)
 	}
 
 	loger.Blankln(display.DrawSaveExcel())
@@ -159,30 +139,23 @@ func workerFicheAppuiFt() {
 		loger.BlankDateln(fmt.Sprintf("N°%v | Files: %s", job.Id, filepath.Base(job.Path)))
 
 		excelFile := job.Path
-		f, err := xlsx.OpenFile(excelFile)
+		f, err := excelize.OpenFile(excelFile)
 		if err != nil {
 			loger.Errorln(fmt.Sprintf("Crash with this files: %s", filepath.Base(excelFile)))
 			wg.Done()
 			continue
 		}
 
-		sht := f.Sheets[0]
+		sht := f.GetSheetName(f.GetActiveSheetIndex())
 
-		verifFA, _ := getCellValue(sht, 0, 0)
-		if !strings.Contains(task.StrToLower(verifFA), "appui") {
-			loger.Errorln(fmt.Sprintf("Ce n'est pas le bon format de fiche appui: %s", filepath.Base(excelFile)))
-			wg.Done()
-			continue
-		}
+		adresse, _ := f.GetCellValue(sht, "D5")
+		ville, _ := f.GetCellValue(sht, "D4")
+		numAppui, _ := f.GetCellValue(sht, "D3")
+		type1, _ := f.GetCellValue(sht, "C26")
+		typeNApp, _ := f.GetCellValue(sht, "M52")
+		natureTvx, _ := f.GetCellValue(sht, "M53")
 
-		adresse, _ := getCellValue(sht, 4, 3)
-		ville, _ := getCellValue(sht, 3, 3)
-		numAppui, _ := getCellValue(sht, 2, 3)
-		type1, _ := getCellValue(sht, 25, 2)
-		typeNApp, _ := getCellValue(sht, 51, 12)
-		natureTvx, _ := getCellValue(sht, 52, 12)
-
-		etiquetteJaune, _ := getCellValue(sht, 11, 20)
+		etiquetteJaune, _ := f.GetCellValue(sht, "U12")
 		switch task.StrToLower(etiquetteJaune) {
 		case "oui":
 			etiquetteJaune = "non"
@@ -190,51 +163,48 @@ func workerFicheAppuiFt() {
 			etiquetteJaune = "oui"
 		}
 
-		effort1, rgb1 := getCellValue(sht, 25, 18)
-		effort2, rgb2 := getCellValue(sht, 25, 20)
-		effort3, rgb3 := getCellValue(sht, 25, 22)
+		effort1, _ := f.GetCellValue(sht, "S26")
+		effort2, _ := f.GetCellValue(sht, "U26")
+		effort3, _ := f.GetCellValue(sht, "W26")
 
-		lat, _ := getCellValue(sht, 4, 15)
-		lon, _ := getCellValue(sht, 5, 15)
-		operateur, _ := getCellValue(sht, 2, 9)
-		utilisableEnEtat, _ := getCellValue(sht, 11, 22)
-		environnement, _ := getCellValue(sht, 51, 22)
-		commentaireEtatAppui, _ := getCellValue(sht, 12, 5)
-		commentaireGlobal, _ := getCellValue(sht, 54, 0)
-		proxiEnedis, _ := getCellValue(sht, 52, 22)
+		lat, _ := f.GetCellValue(sht, "P5")
+		lon, _ := f.GetCellValue(sht, "P6")
+		operateur, _ := f.GetCellValue(sht, "J3")
+		utilisableEnEtat, _ := f.GetCellValue(sht, "W12")
+		environnement, _ := f.GetCellValue(sht, "W52")
+		commentaireEtatAppui, _ := f.GetCellValue(sht, "F13")
+		commentaireGlobal, _ := f.GetCellValue(sht, "A55")
+		proxiEnedis, _ := f.GetCellValue(sht, "W53")
 
-		insee, _ := getCellValue(sht, 3, 21)
+		insee, _ := f.GetCellValue(sht, "V4")
 		idMetier := fmt.Sprintf("%s/%s", numAppui, insee)
-		date, _ := getCellValue(sht, 0, 19)
-		pb, _ := getCellValue(sht, 17, 13)
+
+		date, _ := f.GetCellValue(sht, "T1")
+		pb, _ := f.GetCellValue(sht, "N18")
 
 		// insert value
-		setCellValue(job.Id, 0, job.Path)
-		setCellValue(job.Id, 1, adresse)
-		setCellValue(job.Id, 2, ville)
-		setCellValue(job.Id, 3, numAppui)
-		setCellValue(job.Id, 4, type1)
-		setCellValue(job.Id, 5, typeNApp)
-		setCellValue(job.Id, 6, natureTvx)
-		setCellValue(job.Id, 7, etiquetteJaune)
-		setCellValue(job.Id, 8, effort1)
-		setCellValue(job.Id, 9, effort2)
-		setCellValue(job.Id, 10, effort3)
-		setCellValue(job.Id, 11, lat)
-		setCellValue(job.Id, 12, lon)
-		setCellValue(job.Id, 13, operateur)
-		setCellValue(job.Id, 14, utilisableEnEtat)
-		setCellValue(job.Id, 15, environnement)
-		setCellValue(job.Id, 16, commentaireEtatAppui)
-		setCellValue(job.Id, 17, commentaireGlobal)
-		setCellValue(job.Id, 18, proxiEnedis)
-		setCellValue(job.Id, 19, idMetier)
-		setCellValue(job.Id, 20, date)
-		setCellValue(job.Id, 21, pb)
-
-		setCellBgColor(job.Id, 8, rgb1)
-		setCellBgColor(job.Id, 9, rgb2)
-		setCellBgColor(job.Id, 10, rgb3)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("A%v", job.Id), job.Path)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("B%v", job.Id), adresse)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("C%v", job.Id), ville)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("D%v", job.Id), numAppui)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("E%v", job.Id), type1)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("F%v", job.Id), typeNApp)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("G%v", job.Id), natureTvx)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("H%v", job.Id), etiquetteJaune)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("I%v", job.Id), effort1)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("J%v", job.Id), effort2)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("K%v", job.Id), effort3)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("L%v", job.Id), lat)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("M%v", job.Id), lon)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("N%v", job.Id), operateur)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("O%v", job.Id), utilisableEnEtat)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("P%v", job.Id), environnement)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("Q%v", job.Id), commentaireEtatAppui)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("R%v", job.Id), commentaireGlobal)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("S%v", job.Id), proxiEnedis)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("T%v", job.Id), idMetier)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("U%v", job.Id), date)
+		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("V%v", job.Id), pb)
 
 		wg.Done()
 	}
