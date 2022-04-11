df55e7f11ced52e260533ee1055dfced71a93e16
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-11 21:39:41 +0200

    correction bug excel loop strcut

--- pkg/compiler.go
+++ pkg/compiler.go
@@ -6,7 +6,6 @@ import (
 	"FilesDIR/loger"
 	"FilesDIR/task"
 	"fmt"
-	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/tealeg/xlsx"
 	"io/ioutil"
 	"os"
@@ -21,11 +20,41 @@ type compilData struct {
 	Id   int
 }
 
+type DataExp struct {
+	path                 string
+	id                   int
+	adresse              string
+	ville                string
+	numAppui             string
+	type1                string
+	typeNApp             string
+	natureTvx            string
+	etiquetteJaune       string
+	effort1              string
+	effort2              string
+	effort3              string
+	lat                  string
+	lon                  string
+	operateur            string
+	utilisableEnEtat     string
+	environnement        string
+	commentaireEtatAppui string
+	commentaireGlobal    string
+	proxiEnedis          string
+	idMetier             string
+	date                 string
+	pb                   string
+}
+
 var (
-	wg   sync.WaitGroup
-	jobs = make(chan compilData)
-	Wb   = &excelize.File{}
-	Id   int
+	wg         sync.WaitGroup
+	wgWritter  sync.WaitGroup
+	jobs       = make(chan compilData)
+	jobsWrtier = make(chan int)
+	Wb         = &xlsx.File{}
+	Sht        = &xlsx.Sheet{}
+	Data       []DataExp
+	Id         int
 )
 
 //...
@@ -46,29 +75,53 @@ func CompilerFicheAppuiFt(path string) {
 
 	Id = 1
 
-	Wb = excelize.NewFile()
-	_ = Wb.SetCellValue("Sheet1", "A1", "Chemin de la fiche")
-	_ = Wb.SetCellValue("Sheet1", "B1", "Adresse")
-	_ = Wb.SetCellValue("Sheet1", "C1", "Ville")
-	_ = Wb.SetCellValue("Sheet1", "D1", "Num appui")
-	_ = Wb.SetCellValue("Sheet1", "E1", "Type appui")
-	_ = Wb.SetCellValue("Sheet1", "F1", "Type_n_app")
-	_ = Wb.SetCellValue("Sheet1", "G1", "Nature TVX")
-	_ = Wb.SetCellValue("Sheet1", "H1", "Etiquette jaune")
-	_ = Wb.SetCellValue("Sheet1", "I1", "Effort avant ajout câble")
-	_ = Wb.SetCellValue("Sheet1", "J1", "Effort après ajout câble")
-	_ = Wb.SetCellValue("Sheet1", "K1", "Effort nouveau appui")
-	_ = Wb.SetCellValue("Sheet1", "L1", "Latitude")
-	_ = Wb.SetCellValue("Sheet1", "M1", "Longitude")
-	_ = Wb.SetCellValue("Sheet1", "N1", "Opérateur")
-	_ = Wb.SetCellValue("Sheet1", "O1", "Appui utilisable en l'état")
-	_ = Wb.SetCellValue("Sheet1", "P1", "Environnement")
-	_ = Wb.SetCellValue("Sheet1", "Q1", "Commentaire appui")
-	_ = Wb.SetCellValue("Sheet1", "R1", "Commentaire global")
-	_ = Wb.SetCellValue("Sheet1", "S1", "Proxi ENEDIS")
-	_ = Wb.SetCellValue("Sheet1", "T1", "id_metier_")
-	_ = Wb.SetCellValue("Sheet1", "U1", "Date")
-	_ = Wb.SetCellValue("Sheet1", "V1", "PB")
+	Wb = xlsx.NewFile()
+	Sht, _ = Wb.AddSheet("Sheet1")
+
+	col0, _ := Sht.Cell(0, 0)
+	col0.SetValue("Chemin de la fiche")
+	col1, _ := Sht.Cell(0, 1)
+	col1.SetValue("Adresse")
+	col2, _ := Sht.Cell(0, 2)
+	col2.SetValue("Ville")
+	col3, _ := Sht.Cell(0, 3)
+	col3.SetValue("Num appui")
+	col4, _ := Sht.Cell(0, 4)
+	col4.SetValue("Type appui")
+	col5, _ := Sht.Cell(0, 5)
+	col5.SetValue("Type_n_app")
+	col6, _ := Sht.Cell(0, 6)
+	col6.SetValue("Nature TVX")
+	col7, _ := Sht.Cell(0, 7)
+	col7.SetValue("Etiquette jaune")
+	col8, _ := Sht.Cell(0, 8)
+	col8.SetValue("Effort avant ajout câble")
+	col9, _ := Sht.Cell(0, 9)
+	col9.SetValue("Effort après ajout câble")
+	col10, _ := Sht.Cell(0, 10)
+	col10.SetValue("Effort nouveau appui")
+	col11, _ := Sht.Cell(0, 11)
+	col11.SetValue("Latitude")
+	col12, _ := Sht.Cell(0, 12)
+	col12.SetValue("Longitude")
+	col13, _ := Sht.Cell(0, 13)
+	col13.SetValue("Opérateur")
+	col14, _ := Sht.Cell(0, 14)
+	col14.SetValue("Appui utilisable en l'état")
+	col15, _ := Sht.Cell(0, 15)
+	col15.SetValue("Environnement")
+	col16, _ := Sht.Cell(0, 16)
+	col16.SetValue("Commentaire appui")
+	col17, _ := Sht.Cell(0, 17)
+	col17.SetValue("Commentaire global")
+	col18, _ := Sht.Cell(0, 18)
+	col18.SetValue("Proxi ENEDIS")
+	col19, _ := Sht.Cell(0, 19)
+	col19.SetValue("id_metier_")
+	col20, _ := Sht.Cell(0, 20)
+	col20.SetValue("Date")
+	col21, _ := Sht.Cell(0, 21)
+	col21.SetValue("PB")
 
 	for w := 1; w <= 10; w++ {
 		go workerFicheAppuiFt()
@@ -115,12 +168,27 @@ func CompilerFicheAppuiFt(path string) {
 	wg.Wait()
 	time.Sleep(1 * time.Second)
 
+	iMax := len(Data)
+	for w := 1; w <= 10; w++ {
+		go writeExcelLineWorker()
+	}
+	// Run writing loop
+	for i := 0; i < iMax; i++ {
+		i := i
+		go func() {
+			wgWritter.Add(1)
+			jobsWrtier <- i
+		}()
+	}
+
+	wgWritter.Wait()
+
 	loger.BlankDateln(display.DrawEndCompiler())
 
 	loger.BlankDateln(fmt.Sprintf("Nombre de fiches compilées : %v", Id-1))
 	time.Sleep(800 * time.Millisecond)
 
-	if err := Wb.SaveAs(filepath.Join(path, fmt.Sprintf("__COMPILATION__%v.xlsx", time.Now().Format("20060102150405")))); err != nil {
+	if err := Wb.Save(filepath.Join(path, fmt.Sprintf("__COMPILATION__%v.xlsx", time.Now().Format("20060102150405")))); err != nil {
 		fmt.Println(err)
 	}
 
@@ -179,30 +247,133 @@ func workerFicheAppuiFt() {
 		date, _ := sht.Cell(0, 19)
 		pb, _ := sht.Cell(17, 13)
 
+		Data = append(Data, DataExp{
+			path:                 job.Path,
+			id:                   job.Id,
+			adresse:              adresse.Value,
+			ville:                ville.Value,
+			numAppui:             numAppui.Value,
+			type1:                type1.Value,
+			typeNApp:             typeNApp.Value,
+			natureTvx:            natureTvx.Value,
+			etiquetteJaune:       etiquetteJaune,
+			effort1:              effort1.Value,
+			effort2:              effort2.Value,
+			effort3:              effort3.Value,
+			lat:                  lat.Value,
+			lon:                  lon.Value,
+			operateur:            operateur.Value,
+			utilisableEnEtat:     utilisableEnEtat.Value,
+			environnement:        environnement.Value,
+			commentaireEtatAppui: commentaireEtatAppui.Value,
+			commentaireGlobal:    commentaireGlobal.Value,
+			proxiEnedis:          proxiEnedis.Value,
+			idMetier:             idMetier,
+			date:                 date.Value,
+			pb:                   pb.Value,
+		})
+
 		// insert value
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("A%v", job.Id), job.Path)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("B%v", job.Id), adresse.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("C%v", job.Id), ville.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("D%v", job.Id), numAppui.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("E%v", job.Id), type1.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("F%v", job.Id), typeNApp.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("G%v", job.Id), natureTvx.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("H%v", job.Id), etiquetteJaune)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("I%v", job.Id), effort1.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("J%v", job.Id), effort2.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("K%v", job.Id), effort3.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("L%v", job.Id), lat.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("M%v", job.Id), lon.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("N%v", job.Id), operateur.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("O%v", job.Id), utilisableEnEtat.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("P%v", job.Id), environnement.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("Q%v", job.Id), commentaireEtatAppui.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("R%v", job.Id), commentaireGlobal.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("S%v", job.Id), proxiEnedis.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("T%v", job.Id), idMetier)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("U%v", job.Id), date.Value)
-		_ = Wb.SetCellValue("Sheet1", fmt.Sprintf("V%v", job.Id), pb.Value)
+		//col0, _ := Sht.Cell(job.Id, 0)
+		//col0.SetValue(job.Path)
+		//col1, _ := Sht.Cell(job.Id, 1)
+		//col1.SetValue(adresse.Value)
+		//col2, _ := Sht.Cell(job.Id, 2)
+		//col2.SetValue(ville.Value)
+		//col3, _ := Sht.Cell(job.Id, 3)
+		//col3.SetValue(numAppui.Value)
+		//col4, _ := Sht.Cell(job.Id, 4)
+		//col4.SetValue(type1.Value)
+		//col5, _ := Sht.Cell(job.Id, 5)
+		//col5.SetValue(typeNApp.Value)
+		//col6, _ := Sht.Cell(job.Id, 6)
+		//col6.SetValue(natureTvx.Value)
+		//col7, _ := Sht.Cell(job.Id, 7)
+		//col7.SetValue(etiquetteJaune)
+		//col8, _ := Sht.Cell(job.Id, 8)
+		//col8.SetValue(effort1.Value)
+		//col9, _ := Sht.Cell(job.Id, 9)
+		//col9.SetValue(effort2.Value)
+		//col10, _ := Sht.Cell(job.Id, 10)
+		//col10.SetValue(effort3.Value)
+		//col11, _ := Sht.Cell(job.Id, 11)
+		//col11.SetValue(lat.Value)
+		//col12, _ := Sht.Cell(job.Id, 12)
+		//col12.SetValue(lon.Value)
+		//col13, _ := Sht.Cell(job.Id, 13)
+		//col13.SetValue(operateur.Value)
+		//col14, _ := Sht.Cell(job.Id, 14)
+		//col14.SetValue(utilisableEnEtat.Value)
+		//col15, _ := Sht.Cell(job.Id, 15)
+		//col15.SetValue(environnement.Value)
+		//col16, _ := Sht.Cell(job.Id, 16)
+		//col16.SetValue(commentaireEtatAppui.Value)
+		//col17, _ := Sht.Cell(job.Id, 17)
+		//col17.SetValue(commentaireGlobal.Value)
+		//col18, _ := Sht.Cell(job.Id, 18)
+		//col18.SetValue(proxiEnedis.Value)
+		//col19, _ := Sht.Cell(job.Id, 19)
+		//col19.SetValue(idMetier)
+		//col20, _ := Sht.Cell(job.Id, 20)
+		//col20.SetValue(date.Value)
+		//col21, _ := Sht.Cell(job.Id, 21)
+		//col21.SetValue(pb.Value)
 
 		wg.Done()
+	}
+}
+
+func writeExcelLineWorker() {
+	for jobb := range jobsWrtier {
+
+		fmt.Print("\r")
+		fmt.Printf("\rSauvegarde du fichier Excel...  %v/%v", jobb, len(Data))
+
+		col0, _ := Sht.Cell(jobb, 0)
+		col0.SetValue(Data[jobb].path)
+		col1, _ := Sht.Cell(jobb, 1)
+		col1.SetValue(Data[jobb].adresse)
+		col2, _ := Sht.Cell(jobb, 2)
+		col2.SetValue(Data[jobb].ville)
+		//col3, _ := Sht.Cell(job, 3)
+		//col3.SetValue(numAppui.Value)
+		//col4, _ := Sht.Cell(job, 4)
+		//col4.SetValue(type1.Value)
+		//col5, _ := Sht.Cell(job, 5)
+		//col5.SetValue(typeNApp.Value)
+		//col6, _ := Sht.Cell(job, 6)
+		//col6.SetValue(natureTvx.Value)
+		//col7, _ := Sht.Cell(job, 7)
+		//col7.SetValue(etiquetteJaune)
+		//col8, _ := Sht.Cell(job, 8)
+		//col8.SetValue(effort1.Value)
+		//col9, _ := Sht.Cell(job, 9)
+		//col9.SetValue(effort2.Value)
+		//col10, _ := Sht.Cell(job, 10)
+		//col10.SetValue(effort3.Value)
+		//col11, _ := Sht.Cell(job, 11)
+		//col11.SetValue(lat.Value)
+		//col12, _ := Sht.Cell(job, 12)
+		//col12.SetValue(lon.Value)
+		//col13, _ := Sht.Cell(job, 13)
+		//col13.SetValue(operateur.Value)
+		//col14, _ := Sht.Cell(job, 14)
+		//col14.SetValue(utilisableEnEtat.Va
+		//col15, _ := Sht.Cell(job, 15)
+		//col15.SetValue(environnement.Value
+		//col16, _ := Sht.Cell(job, 16)
+		//col16.SetValue(commentaireEtatAppu
+		//col17, _ := Sht.Cell(job, 17)
+		//col17.SetValue(commentaireGlobal.V
+		//col18, _ := Sht.Cell(job, 18)
+		//col18.SetValue(proxiEnedis.Value)
+		//col19, _ := Sht.Cell(job, 19)
+		//col19.SetValue(idMetier)
+		//col20, _ := Sht.Cell(job, 20)
+		//col20.SetValue(date.Value)
+		//col21, _ := Sht.Cell(job, 21)
+		//col21.SetValue(pb.Value)
+
+		wgWritter.Done()
 	}
 }
