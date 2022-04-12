184f6c057b2c05ad96bb7878626cc419aad44a02
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-12 20:03:56 +0200

    Couleur mef

--- FilesDIR.go
+++ FilesDIR.go
@@ -73,7 +73,7 @@ func main() {
 		f.DrawEnd(s.SrcPath, s.DstPath, s.ReqFinal, s.NbGoroutine, s.NbFiles, s.TimerSearch, timerEnd)
 	}
 
-	color.Print("<cyan>Appuyer sur Entrée pour quitter...</>")
+	color.Printf("<fg=%[1]s>Appuyer sur Entrée pour quitter...</>", globals.Th2)
 	_, err := bufio.NewReader(os.Stdin).ReadBytes('\n')
 	if err != nil {
 		loger.Crashln(err)
