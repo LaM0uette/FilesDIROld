367816cd5637f4acaf35692e983d91c708f923ee
LaM0uette <lam0uette.dev@gmail.com>
2022-04-13 14:21:45 +0200

    affichage du logo principal

--- loger/loger.go
+++ loger/loger.go
@@ -15,7 +15,7 @@ var (
 )
 
 func init() {
-	logFile, err := os.OpenFile(filepath.Join(globals.FolderLogs, fmt.Sprintf("SLog_%v.txt", time.Now().Format("20060102150405"))), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logFile, err := os.OpenFile(filepath.Join(globals.FolderLogs, fmt.Sprintf("SLog_%v.log", time.Now().Format("20060102150405"))), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
